d09ab4821f3aebbd7b64bbdedbefbf55761bc98f
Hung-Tao Chou <CHOU_H@ncf.wdc.com>
2016-10-21 11:03:18 -0700

    final project ipython notebook done

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -37,7 +37,7 @@ def select_features(features, selects):
 
             
             
-def test_clf(clf, labels, features, folds = 100):
+def test_clf(clf, labels, features, folds = 1000):
 
     cv = StratifiedShuffleSplit(labels, folds, random_state = 42)
     c_matrix = [[0,0],[0,0]]
@@ -59,25 +59,12 @@ def test_clf(clf, labels, features, folds = 100):
         
         c_matrix+=confusion_matrix(labels_test, predictions)
         
-#        for prediction, truth in zip(predictions, labels_test):
-#            if prediction == 0 and truth == 0:
-#                true_negatives += 1
-#            elif prediction == 0 and truth == 1:
-#                false_negatives += 1
-#            elif prediction == 1 and truth == 0:
-#                false_positives += 1
-#            elif prediction == 1 and truth == 1:
-#                true_positives += 1
-#            else:
-#                print "Warning: Found a predicted label not == 0 or 1."
-#                print "All predictions should take value 0 or 1."
-#                print "Evaluating performance for processed predictions:"
-#                break
+
     try:
         true_negatives = c_matrix[0][0]
         false_negatives = c_matrix[1][0]
         false_positives = c_matrix[0][1]
-        true_positives = c_matrix[1][1]      
+        true_positives = c_matrix[1][1]
         total_predictions = true_negatives + false_negatives + false_positives + true_positives
         accuracy = 1.0*(true_positives + true_negatives)/total_predictions
         precision = 1.0*true_positives/(true_positives+false_positives)
@@ -86,6 +73,7 @@ def test_clf(clf, labels, features, folds = 100):
 #        f2 = (1+2.0*2.0) * precision*recall/(4*precision + recall)
         print clf
         print "accuracy: {}, precison: {}, \n recall: {}, f1: {}".format(accuracy, precision, recall, f1, display_precision = 5)
+        print "true positive: {}, false positive: {}, \n false negative: {}, true negative: {}".format(true_positives, false_positives, false_negatives, true_negatives)
 #        print RESULTS_FORMAT_STRING.format(total_predictions, true_positives, false_positives, false_negatives, true_negatives)
         print ""
     except:
@@ -194,11 +182,43 @@ for i, feature1 in enumerate(features_list2[1:]):
 figure.subplots_adjust(left=.02, right=.98)
 plt.show()
 
-outlier_name =  str(df['name'][df.salary ==df['salary'].max()])
-print outlier_name
+outlier_name1 =  str(df['name'][df.salary ==df['salary'].max()])
+outlier_name2 =  str(df['name'][df.bonus ==df['bonus'].max()])
+outlier_name3 =  str(df['name'][df.other ==df['other'].max()])
+print outlier_name1, outlier_name2, outlier_name3
 
-df = df[df.name != outlier_name]
+df = df[df.salary !=df['salary'].max()]
 print my_dataset.pop('TOTAL',0)
+
+
+figure = plt.figure(figsize=(27, 20))
+j = 1
+for i, feature1 in enumerate(features_list2[1:]):
+    
+
+    y_name = features_list2[0]
+    key = feature1
+    bin_step = 20
+    all_data = df[[y_name, key]]    
+    # Remove NaN values from Age data
+    all_data = all_data[~np.isnan(all_data[key])]        
+    # Divide the range of data into bins and count survival rates
+    min_value = all_data[key].min()
+    max_value = all_data[key].max()
+    value_range = max_value - min_value
+    bins = np.arange(min_value, max_value +  value_range/bin_step, value_range/bin_step )
+    y0 = all_data[all_data[y_name] == 0][key].reset_index(drop = True)
+    y1 = all_data[all_data[y_name] == 1][key].reset_index(drop = True)
+    ax = plt.subplot(len(features_list2)/4+1,  4, j)
+    ax.hist(y0, bins = bins, alpha = 0.6, color = 'red', label = 'y0')
+    ax.hist(y1, bins = bins, alpha = 0.6, color = 'green', label = 'y1')
+    ax.set_xlim(bins.min(), bins.max())
+    ax.set_title(key)
+#    ax.legend(framealpha = 0.8)
+    j+=1
+    
+figure.subplots_adjust(left=.02, right=.98)
+plt.show()
 
 
 ### Task 4: Try a varity of classifiers
@@ -209,7 +229,9 @@ print my_dataset.pop('TOTAL',0)
 
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.naive_bayes import GaussianNB
+from sklearn.grid_search import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_selection import RFE, RFECV
@@ -219,40 +241,102 @@ from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_sc
 
 features =df[features_list2[1:]]
 features = features.fillna(features.mean())
+
+for name in df.name:
+    if name in my_dataset.keys():
+        for columns in features.columns:
+            my_dataset[name][columns] = float(df[columns][df.name == name])
+
 labels =df[features_list2[0]]
 labels = np.array(labels)
 features = np.array(features)
 
 
-clf_DT = DecisionTreeClassifier(random_state= 35)
-clf_rf =   RandomForestClassifier(n_estimators=20, max_features='auto', class_weight={1: 10, 0:1}, random_state=24)
-clf_AdaB =  AdaBoostClassifier(n_estimators=100, learning_rate=1.0, random_state=15)
 ### RFECV method
-selectorCV_DT = RFECV(clf_DT, step=1, cv=10, scoring = 'f1')
 
+### RFECV method and try 4 different classifier method: logistic regression, Decision Tree, Random Forrest and Adaptive boosting 
+
+### logistic regression
+
+clf_Log = LogisticRegression(random_state = 14, C= 5, class_weight='balanced')
+selectorCV_Log = RFECV(clf_Log, step=1, cv=5, scoring = 'f1')
+selectorCV_Log.fit(features, labels)
+refcv_figure(selectorCV_Log)
+clf = selectorCV_Log.estimator_
+features_new = selectorCV_Log.transform(features)
+test_clf(clf, labels, features_new, folds = 1000)
+
+param_grid = {"C": [0.01, 0.1, 1, 5, 10, 100, 1000],
+              "penalty" : ['l1', 'l2']
+        }
+
+clf_Log_searchCV = GridSearchCV(clf, param_grid, scoring ='f1', cv=10)
+clf_Log_searchCV.fit(features, labels)
+print clf_Log_searchCV.best_estimator_
+test_clf(clf_Log_searchCV.best_estimator_, labels, features_new, folds = 1000)
+
+
+### Decision Tree
+
+clf_DT = DecisionTreeClassifier(random_state= 32, class_weight='balanced')
+selectorCV_DT = RFECV(clf_DT, step=1, cv=5, scoring = 'f1')
 selectorCV_DT.fit(features, labels)
-#print selectorCV_DT.support_
-#print selectorCV_DT.ranking_
 refcv_figure(selectorCV_DT)
 clf = selectorCV_DT.estimator_
-test_clf(clf, labels, features[:,selectorCV_DT.support_], folds = 100)
+features_new = selectorCV_DT.transform(features)
+test_clf(clf, labels, features_new, folds = 1000)
+#parameters={'min_samples_split' : range(2,20,2),'max_depth': range(1,10,2), 'criterion': ['gini', 'entropy']}
+param_grid = {"max_depth": [1,4,7, None],
+              "max_features": [1, 3, 5, 7],
+              "min_samples_split": [1, 2, 4, 7],
+              "min_samples_leaf": [1, 3, 10],
+              "criterion": ["gini", "entropy"]}
+clf_DT_searchCV = GridSearchCV(clf, param_grid, scoring ='f1', cv=10)
+clf_DT_searchCV.fit(features, labels)
+print clf_DT_searchCV.best_estimator_,
+test_clf(clf_DT_searchCV.best_estimator_, labels, features_new, folds = 1000)
 
 
-selectorCV_rf = RFECV(clf_rf, step=1, cv=10, scoring = 'f1')
+
+### Random Forrest
+
+clf_rf =   RandomForestClassifier(n_estimators=20, max_features='auto', class_weight='balanced', random_state=24)
+selectorCV_rf = RFECV(clf_rf, step=1, cv=5, scoring = 'f1')
 selectorCV_rf.fit(features, labels)
-#print selectorCV_rf.support_
-#print selectorCV_rf.ranking_
 refcv_figure(selectorCV_rf)
 clf = selectorCV_rf.estimator_
-test_clf(clf, labels, features[:,selectorCV_rf.support_], folds = 100)
+features_new = selectorCV_rf.transform(features)
+test_clf(clf, labels, features_new, folds = 1000)
+param_grid = {
+    "n_estimators": range(20,50,300),
+    "max_features": ['auto', 'sqrt', 'log2'],
+    "min_samples_split": [1, 2, 4, 7],
+    "min_samples_leaf": [1, 3, 10],
+    "criterion": ["gini", "entropy"]}    
+clf_rf_searchCV = GridSearchCV(clf, param_grid, scoring = 'f1', cv = 10)
+clf_rf_searchCV.fit(features, labels)
+print clf_rf_searchCV.best_estimator_,
+test_clf(clf_rf_searchCV.best_estimator_, labels, features_new, folds = 1000)
 
+
+### Adaptive boosting
+
+DTC = DecisionTreeClassifier(random_state = 11, class_weight = "balanced")
+clf_AdaB =  AdaBoostClassifier(base_estimator = DTC, n_estimators=50, learning_rate=1.0, random_state=25)
 selectorCV_AdaB = RFECV(clf_AdaB, step=1, cv=10, scoring = 'f1')
 selectorCV_AdaB.fit(features, labels)
-#print selectorCV_AdaB.support_
-#print selectorCV_AdaB.ranking_
 refcv_figure(selectorCV_AdaB)
 clf = selectorCV_AdaB.estimator_
-test_clf(clf, labels, features[:,selectorCV_AdaB.support_], folds = 100)
+features_new = selectorCV_AdaB.transform(features)
+test_clf(clf, labels, features_new, folds = 1000)
+param_grid = {
+    "n_estimators": range(20,50,300),
+    "learning_rate": [0.1, 1, 10, 100],
+    "algorithm": ['SAMME', 'SAMME.R']}    
+clf_AdaB_searchCV = GridSearchCV(clf, param_grid, scoring = 'f1', cv=10)
+clf_AdaB_searchCV.fit(features, labels)
+print clf_AdaB_searchCV.best_estimator_,
+test_clf(clf_AdaB_searchCV.best_estimator_, labels, features_new, folds = 1000)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -273,49 +357,30 @@ from sklearn.cross_validation import StratifiedShuffleSplit
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 import numpy as np
 
-features =df[features_list2[1:]]
-features = features.fillna(features.mean())
-labels =df[features_list2[0]]
-labels = np.array(labels)
-features = np.array(features)
-sss = StratifiedShuffleSplit(labels,  n_iter=10, test_size=0.1, random_state=21)
-clf_val = DecisionTreeClassifier()
-clf_rf =   RandomForestClassifier(max_depth=5, n_estimators=10, max_features=4, class_weight={1: 10, 0:1})
-clf_AdaB =  AdaBoostClassifier(n_estimators=100, learning_rate=2.0)
 
-for train_index, test_index in sss:
-    train_index = train_index
-    X_train, X_test = features[train_index], features[test_index]
-    y_train, y_test = labels[train_index], labels[test_index]
-    
-#    clf_val.fit(X_train, y_train)
-#    pred = clf_val.predict(X_test)
-#
-#    print "stratified split test:", clf_val.score(X_test, y_test)
-#    print 'precision = ', precision_score(y_test,pred)
-#    print 'recall = ', recall_score(y_test,pred)
-    
-    clf_rf.fit(X_train, y_train)
-    pred = clf_rf.predict(X_test)
-    
-#    print "random forrest stratified split test:", clf_rf.score(X_test, y_test)
-#    print 'precision = ', precision_score(y_test,pred)
-#    print 'recall = ', recall_score(y_test,pred)
-    
-    clf_AdaB.fit(X_train, y_train)
-    pred = clf_AdaB.predict(X_test)
-#    clf = clf_AdaB
-    
-#    print "adaBoost stratified split test:", clf_AdaB.score(X_test, y_test)
-#    print 'precision = ', precision_score(y_test,pred)
-#    print 'recall = ', recall_score(y_test,pred)
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-    
-    
+
+ 
+clf = clf_Log_searchCV.best_estimator_
+features_selected = select_features(features_list2[1:], selectorCV_Log.support_)
+print 'CV_Log:'
+print features_selected
+dump_classifier_and_data(clf, my_dataset, features_selected)
+tester.main()
+   
+clf = clf_DT_searchCV.best_estimator_
+features_selected = select_features(features_list2[1:], selectorCV_DT.support_)
+print 'CV_DT:'
+print features_selected
+dump_classifier_and_data(clf, my_dataset, features_selected)
+tester.main()
+
+
 clf = selectorCV_rf.estimator_
+clf = clf_rf_searchCV.best_estimator_
 features_selected = select_features(features_list2[1:], selectorCV_rf.support_)
 print 'CV_rf:'
 print features_selected
@@ -324,15 +389,9 @@ tester.main()
 
 
 clf = selectorCV_AdaB.estimator_
+clf = clf_AdaB_searchCV.best_estimator_
 features_selected = select_features(features_list2[1:], selectorCV_AdaB.support_)
 print 'CV_AdaB:'
-print features_selected
-dump_classifier_and_data(clf, my_dataset, features_selected)
-tester.main()
-
-clf = selectorCV_DT.estimator_
-features_selected = select_features(features_list2[1:], selectorCV_DT.support_)
-print 'CV_DT:'
 print features_selected
 dump_classifier_and_data(clf, my_dataset, features_selected)
 tester.main()
